3064cdedd1efb45bd3ef51f04c5e869096a84946
Jonathan-Greve <jonagrev@gmail.com>
2022-12-19 08:25:45 +0800

    Render connected clients

--- ShowClientsConnected.h
+++ ShowClientsConnected.h
@@ -6,20 +6,19 @@
 class ShowClientsConnected
 {
 public:
-    void operator()(ConnectionData& connection_data)
+    void operator()(ConnectionData& connection_data, const std::unordered_map<int, GW_skill> skills)
     {
-        const auto connections = connection_data.get_connected_client_emails();
+        const auto connected_client_names = connection_data.get_connected_clients_shared_memory_names();
 
         ImGui::Begin("Connected clients");
 
         ImGui::Text("Number of connections: ");
         ImGui::SameLine(0, 0);
-        ImGui::TextColored(ImVec4(0.2, 1, 0.3, 1), std::to_string(connections.size()).c_str());
+        ImGui::TextColored(ImVec4(0.2, 1, 0.3, 1), std::to_string(connected_client_names.size()).c_str());
 
-        if (connections.size() > 0)
+        for (const auto& name : connected_client_names)
         {
-            auto first_conn = *connections.begin();
-            if (ImGui::CollapsingHeader(first_conn.c_str(), ImGuiTreeNodeFlags_OpenOnDoubleClick))
+            if (ImGui::CollapsingHeader(name.c_str(), ImGuiTreeNodeFlags_OpenOnDoubleClick))
             {
                 ImGui::Text("IsItemHovered: %d", ImGui::IsItemHovered());
             }
