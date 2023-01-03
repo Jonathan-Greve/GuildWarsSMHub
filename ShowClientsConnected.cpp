4aeacf87fa8f4f2c4c0b53eaaa104287f5d8cdf8
Jonathan-Greve <jonagrev@gmail.com>
2023-01-03 08:07:36 +0800

    Cleanup + more data shown

--- ShowClientsConnected.cpp
+++ ShowClientsConnected.cpp
@@ -9,46 +9,22 @@ void ShowClientsConnected::operator()(ConnectionData& connection_data,
     {
         const auto connected_client_ids = connection_data.get_connected_client_ids();
 
-        ImGui::Begin("Connected clients");
-
-        ImGui::Text("Number of connections: ");
-        ImGui::SameLine(0, 0);
-        ImGui::TextColored(ImVec4(0.2, 1, 0.3, 1), std::to_string(connected_client_ids.size()).c_str());
-
-        for (const auto& id : connected_client_ids)
-        {
-            if (ImGui::CollapsingHeader(id.c_str(), ImGuiTreeNodeFlags_OpenOnDoubleClick))
-            {
-                ImGui::Text("IsItemHovered: %d", ImGui::IsItemHovered());
-            }
-        }
-        ImGui::End();
-
-        // Using ImGui make a window for showing the number of connections, connection names, character name, character class, location, party_id and party size.
-
-        ImGui::Begin("Connected clients 2");
+        ImGui::Begin("Connected clients info");
         // Display the number of connections
         ImGui::Text("Number of connections:");
         ImGui::SameLine();
         ImGui::TextColored(ImVec4(0.2, 1, 0.3, 1), std::to_string(connected_client_ids.size()).c_str());
 
-        // Display a list of connections
-        if (ImGui::BeginListBox("Connections listbox",
-                                ImVec2(-FLT_MIN, 5 * ImGui::GetTextLineHeightWithSpacing())))
-        {
-            for (const auto& id : connected_client_ids)
-            {
-                ImGui::Text("%s", id.c_str());
-            }
-            ImGui::EndListBox();
-        }
-
         // Display a list of character names, character classes, locations, party IDs, and party sizes
-        ImGui::Columns(6, "connection_columns");
+        ImGui::Columns(8, "connection_columns");
         ImGui::Separator();
         ImGui::Text("Game state");
         ImGui::NextColumn();
+        ImGui::Text("Email");
+        ImGui::NextColumn();
         ImGui::Text("Character Name");
+        ImGui::NextColumn();
+        ImGui::Text("Agent ID");
         ImGui::NextColumn();
         ImGui::Text("Character Class");
         ImGui::NextColumn();
@@ -66,208 +42,84 @@ void ShowClientsConnected::operator()(ConnectionData& connection_data,
             if (client_data)
             {
                 auto game_state_name = GWIPC::EnumNamesGameState()[client_data->game_state()];
-                ImGui::Text("%s", game_state_name);
+                if (game_state_name)
+                {
+                    ImGui::Text("%s", game_state_name);
+                }
                 ImGui::NextColumn();
 
-                auto name = client_data->character()->agent_living()->name();
-                ImGui::Text("%s", name->c_str());
+                ImGui::Text("%s", id.c_str());
                 ImGui::NextColumn();
 
-                auto primary = client_data->character()->agent_living()->primary_profession();
-                auto primary_name = GWIPC::EnumNamesProfession()[primary];
-                auto secondary = client_data->character()->agent_living()->secondary_profession();
-                auto secondary_name = GWIPC::EnumNamesProfession()[secondary];
-                ImGui::Text("%s/%s", primary_name, secondary_name);
+                if (client_data->character() && client_data->character()->agent_living() &&
+                    client_data->character()->agent_living()->agent())
+                {
+                    auto name = client_data->character()->agent_living()->name();
+                    ImGui::Text("%s", name->c_str());
+                    ImGui::NextColumn();
+
+                    ImGui::Text("%u", client_data->character()->agent_living()->agent()->agent_id());
+                    ImGui::NextColumn();
+
+                    auto primary = client_data->character()->agent_living()->primary_profession();
+                    auto primary_name = GWIPC::EnumNamesProfession()[primary];
+                    auto secondary = client_data->character()->agent_living()->secondary_profession();
+                    auto secondary_name = GWIPC::EnumNamesProfession()[secondary];
+                    ImGui::Text("%s/%s", primary_name, secondary_name);
+                    ImGui::NextColumn();
+                }
+                else
+                {
+                    ImGui::NextColumn();
+                    ImGui::NextColumn();
+                    ImGui::NextColumn();
+                }
+
+                int instance_id = -1;
+                if (client_data->instance())
+                {
+                    auto map_id = client_data->instance()->map_id();
+                    ImGui::Text("%s", GW::Constants::NAME_FROM_ID[map_id]);
+
+                    instance_id = client_data->instance()->instance_id();
+                }
                 ImGui::NextColumn();
 
-                auto map_id = client_data->instance()->map_id();
-                ImGui::Text("%s", GW::Constants::NAME_FROM_ID[map_id]);
+                uint32_t num_hero_members = 0;
+                uint32_t num_player_members = 0;
+                uint32_t num_henchman_members = 0;
+                if (client_data->party())
+                {
+                    auto party_id = client_data->party()->party_id();
+                    ImGui::Text("(%u, %u)", instance_id, party_id);
+
+                    auto hero_members = client_data->party()->hero_members();
+                    if (hero_members)
+                    {
+                        num_hero_members += hero_members->size();
+                    }
+
+                    auto player_members = client_data->party()->player_members();
+                    if (player_members)
+                    {
+                        num_player_members += player_members->size();
+                    }
+
+                    auto henchman_members = client_data->party()->henchman_members();
+                    if (henchman_members)
+                    {
+                        num_henchman_members += henchman_members->size();
+                    }
+                }
                 ImGui::NextColumn();
 
-                auto instance_id = client_data->instance()->instance_id();
-                auto party_id = client_data->party()->party_id();
-                ImGui::Text("(%u, %u)", instance_id, party_id);
-                ImGui::NextColumn();
-
-                ImGui::Text("%d", 1);
+                ImGui::Text("%d(A)|%d(P)|%d(H)|%d(h)",
+                            num_hero_members + num_player_members + num_henchman_members, num_player_members,
+                            num_hero_members, num_henchman_members);
                 ImGui::NextColumn();
             }
         }
         ImGui::Columns(1);
-        ImGui::End();
-
-        ImGui::Begin("Draw 2d");
-        // Get the current draw list and the position of the current window
-        ImDrawList* drawList = ImGui::GetWindowDrawList();
-        ImVec2 windowPos = ImGui::GetWindowPos();
-
-        // Define the coordinates of the rectangle, relative to the window
-        ImVec2 p1(10, 10);
-        ImVec2 p2(100, 100);
-
-        // Offset the rectangle coordinates by the window position
-        p1 += windowPos;
-        p2 += windowPos;
-
-        // Set the color of the rectangle
-        ImU32 color = IM_COL32(255, 0, 0, 255);
-
-        // Draw the rectangle
-        drawList->AddRect(p1, p2, color);
-
-        // Check if the mouse is hovering over the rectangle
-        if (ImGui::IsMouseHoveringRect(p1, p2))
-        {
-            // Begin a tooltip
-            ImGui::BeginTooltip();
-
-            // Display the info for the rectangle
-            ImGui::Text("Info for rectangle");
-            ImGui::Separator();
-            ImGui::Text("Top-left corner: (%.1f, %.1f)", p1.x, p1.y);
-            ImGui::Text("Bottom-right corner: (%.1f, %.1f)", p2.x, p2.y);
-
-            // End the tooltip
-            ImGui::EndTooltip();
-        }
-        ImGui::End();
-
-        Character character;
-
-        // Initialize the character data
-        character.name = "Bob";
-        character.health = 100.0f;
-        character.mana = 50.0f;
-        character.skills = {"Skill 1", "Skill 2", "Skill 3", "Skill 4",
-                            "Skill 5", "Skill 6", "Skill 7", "Skill 8"};
-
-        // Create the unit frame
-        ImGui::Begin("Unit Frame");
-
-        // Display the character name
-        ImGui::Text("%s", character.name.c_str());
-
-        // Display the character health and mana
-        ImGui::ProgressBar(character.health / 100.0f, ImVec2(0.0f, 0.0f), "Health");
-        ImGui::SameLine();
-        ImGui::ProgressBar(character.mana / 100.0f, ImVec2(0.0f, 0.0f), "Mana");
-
-        // Display the skillbar
-        ImGui::Separator();
-        for (int i = 0; i < 8; i++)
-        {
-            ImGui::PushID(i);
-            ImGui::Button(character.skills[i].c_str());
-            ImGui::PopID();
-            ImGui::SameLine();
-        }
-        ImGui::End();
-
-        // Set the size constraints for the window
-        ImGui::SetNextWindowSizeConstraints(ImVec2(100, 100), ImVec2(FLT_MAX, FLT_MAX));
-        ImGui::Begin("Unit Frame 2");
-        ImVec2 contentRegion = ImGui::GetContentRegionAvail();
-        // Load the image data for the skill icons
-        ImTextureID skillIcons[8];
-        skillIcons[0] = ImGui::GetIO().Fonts->TexID; // Use the default font as the icon for the first skill
-        //skillIcons[1] = LoadImage("skill2.png"); // Load an image file for the second skill
-        //skillIcons[2] = LoadImage("skill3.png"); // Load an image file for the third skill
-        // ...
-
-        // Create the unit frame
-
-        // Display the character name
-        ImGui::Text("%s", character.name.c_str());
-
-        // Display the character health and mana
-        ImGui::ProgressBar(character.health / 100.0f, ImVec2(0.0f, 0.0f), "Health");
-        ImGui::SameLine();
-        ImGui::ProgressBar(character.mana / 100.0f, ImVec2(0.0f, 0.0f), "Mana");
-
-        // Display the skillbar
-        ImGui::Separator();
-        for (int i = 0; i < 8; i++)
-        {
-            auto skill = skills[i + 1];
-
-            ImGui::PushID(i);
-            ImVec2 buttonSize(contentRegion.x / 10, contentRegion.x / 10);
-            if (ImGui::ImageButton(skill.skill_icon_texture, buttonSize))
-            {
-                // The skill button was clicked, handle the click event here
-            }
-            ImGui::PopID();
-            ImGui::SameLine();
-        }
-        ImGui::End();
-
-        // Set the size constraints for the window
-        ImGui::SetNextWindowSizeConstraints(ImVec2(100, 100), ImVec2(FLT_MAX, FLT_MAX));
-
-        // Set the size constraints for the window
-        ImGui::SetNextWindowSizeConstraints(ImVec2(100, 100), ImVec2(FLT_MAX, FLT_MAX));
-
-        // Begin the window
-        ImGui::Begin("Unit Frame 3");
-
-        // Get the available content region within the window
-        contentRegion = ImGui::GetContentRegionAvail();
-
-        // Calculate the size and position of the name label
-        ImVec2 nameLabelSize(contentRegion.x, 20);
-        ImVec2 nameLabelPos(0, 0);
-
-        // Draw the name label
-        ImGui::SetCursorPos(nameLabelPos);
-        ImGui::Text("Unit Name");
-
-        // Calculate the size and position of the health bar
-        ImVec2 healthBarSize(contentRegion.x, 20);
-        ImVec2 healthBarPos(0, 20);
-
-        // Push the red color style for the health bar
-        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(1.0f, 0.0f, 0.0f, 1.0f));
-
-        // Draw the health bar
-        ImGui::SetCursorPos(healthBarPos);
-        ImGui::ProgressBar(0.5, healthBarSize);
-
-        // Pop the red color style for the health bar
-        ImGui::PopStyleColor();
-
-        // Calculate the size and position of the energy bar
-        ImVec2 energyBarSize(contentRegion.x, 20);
-        ImVec2 energyBarPos(0, 40);
-
-        // Push the blue color style for the energy bar
-        ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.0f, 1.0f, 1.0f));
-
-        // Draw the energy bar
-        ImGui::SetCursorPos(energyBarPos);
-        ImGui::ProgressBar(0.25, energyBarSize);
-
-        // Pop the blue color style for the energy bar
-        ImGui::PopStyleColor();
-
-        // Calculate the size and position of the skill bar
-        ImVec2 skillBarSize(contentRegion.x, 20);
-        ImVec2 skillBarPos(0, 60);
-
-        // Draw the skill bar
-        for (int i = 0; i < 8; i++)
-        {
-            auto skill = skills[i + 1];
-            ImVec2 buttonPos(skillBarPos.x + i * 20, skillBarPos.y);
-            ImGui::PushID(i);
-            if (ImGui::ImageButton(skill.skill_icon_texture, ImVec2(20, 20)))
-            {
-                // Handle button click
-            }
-            ImGui::PopID();
-            ImGui::SameLine();
-        }
-
-        // End the window
         ImGui::End();
 
         // Set the size constraints for the window
@@ -277,19 +129,19 @@ void ShowClientsConnected::operator()(ConnectionData& connection_data,
         ImGui::Begin("Unit Frame 4");
 
         // Get the available content region within the window
-        contentRegion = ImGui::GetContentRegionAvail();
+        auto contentRegion = ImGui::GetContentRegionAvail();
 
         // Calculate the size and position of the name label
-        nameLabelSize = ImVec2(contentRegion.x, 20);
-        nameLabelPos = ImVec2(0, 0);
+        auto nameLabelSize = ImVec2(contentRegion.x, 20);
+        auto nameLabelPos = ImVec2(0, 0);
 
         // Draw the name label
         ImGui::SetCursorPos(nameLabelPos);
         ImGui::Text("Unit Name");
 
         // Calculate the size and position of the health bar
-        healthBarSize = ImVec2(contentRegion.x, 20);
-        healthBarPos = ImVec2(0, 20);
+        auto healthBarSize = ImVec2(contentRegion.x, 20);
+        auto healthBarPos = ImVec2(0, 20);
 
         // Push the red color style for the health bar
         ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(1.0f, 0.0f, 0.0f, 1.0f));
@@ -302,8 +154,8 @@ void ShowClientsConnected::operator()(ConnectionData& connection_data,
         ImGui::PopStyleColor();
 
         // Calculate the size and position of the energy bar
-        energyBarSize = ImVec2(contentRegion.x, 20);
-        energyBarPos = ImVec2(0, 40);
+        auto energyBarSize = ImVec2(contentRegion.x, 20);
+        auto energyBarPos = ImVec2(0, 40);
 
         // Push the blue color style for the energy bar
         ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImVec4(0.0f, 0.0f, 1.0f, 1.0f));
@@ -316,8 +168,8 @@ void ShowClientsConnected::operator()(ConnectionData& connection_data,
         ImGui::PopStyleColor();
 
         // Calculate the size and position of the skill bar
-        skillBarSize = ImVec2(contentRegion.x, 20);
-        skillBarPos = ImVec2(0, 60);
+        auto skillBarSize = ImVec2(contentRegion.x, 20);
+        auto skillBarPos = ImVec2(0, 60);
 
         // Calculate the size of each image button based on the available content region and the number of buttons
         ImVec2 buttonSize(contentRegion.x / 8, contentRegion.x / 8);
